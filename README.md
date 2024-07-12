500ed560bc3a0ebe44497673df0b4f2e6d62d806
Ben Hooper <ben@labrys.io>
2024-07-12 21:54:09 +0200

    feat: got the oracle hooked up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # eth-global-brussels
+
 My hackathon repo for eth global brussels 2024
+
+### Notes
+
+To get the oracle in noir communicating with the outside world:
+
+```
+
+cd client
+bun run dev
+cd ..
+
+cd circuits
+nargo test --oracle-resolver http://localhost:3000/api/hello --show-output
+```
--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { JSONRPCServer } from "json-rpc-2.0";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const server = new JSONRPCServer();
+
+  server.addMethod("getAddressFromRPC", async (params) => {
+    console.log(params);
+
+    return {
+      values: [{ Array: ["0x13E5E5deA5620A8f4B5C430339795cb5BaB6676e"] }],
+    };
+  });
+
+  const jsonRPCRequest = req.body;
+
+  server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+    console.log(jsonRPCResponse);
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      res.status(204);
+    }
+  });
 }
