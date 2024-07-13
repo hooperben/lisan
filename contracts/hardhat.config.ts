b809a8ed729d84b4b039e7f18a45f28c4b79658b
Ben Hooper <ben@labrys.io>
2024-07-13 23:23:55 +0200

    feat: added testnet details

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,6 +4,8 @@ import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 import "dotenv/config";
 
+if (!process.env.PRIVATE_KEY) throw new Error("missing private key brev");
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.24",
@@ -20,8 +22,31 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  networks: {
+    sepolia: {
+      url: process.env.ETHEREUM_JSON_RPC_API_URL_SEPOLIA,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    amoy: {
+      url: process.env.POYLGON_JSON_RPC_API_URL_AMOY,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+    },
+  },
   mocha: {
     timeout: 100_000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: process.env.POLYGON_ETHERSCAN_API_KEY!,
+      sepolia: process.env.ETHERSCAN_API_KEY!,
+    },
   },
 };
 
